8a9c137b2f985c0a5ce8728bdab1423b54dc6f5a
Ilya Zorin <i.zorin@semrush.com>
2021-01-20 16:41:33 +0300

    fix pylint, Gitignore

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
+""" This file needed for heroku, PORT is a heroku var """
 from main import application
+
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=500, ssl_context='adhoc')
+    application.run(host='0.0.0.0', port=PORT, ssl_context='adhoc')
